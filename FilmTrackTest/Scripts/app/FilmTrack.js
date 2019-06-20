3818830918e6ade0fc0b1ff0df444e3918f6d765
Anthony Dizon Duque <adduxe@hotmail.com>
2019-06-20 11:53:47 -0700

    1) Took out troubleshooting lines in the controller.
    2)   Added an input field for the number for people.

--- FilmTrackTest/Scripts/app/FilmTrack.js
+++ FilmTrackTest/Scripts/app/FilmTrack.js
@@ -6,11 +6,15 @@
 
         function ($scope) {
 
-            const INITIAL_STATE = false;  // off
-//            $scope.numOfBulbs = 10;
+            const INITIAL_STATE = false;  // default bulb state: off
+            $scope.numOfBulbs = $scope.numOfPeople = 0;
 
+            $scope.showBulbs = function(){
 
-            $scope.showBulbs = function () {
+                if ($scope.numOfBulbs < 1) {
+                    $scope.bulbs = [];
+                    return;
+                }
 
                 $scope.bulbs = new Array($scope.numOfBulbs);    // Initialize bulbs
 
@@ -18,7 +22,8 @@
                     $scope.bulbs[i] = INITIAL_STATE;
                 }
 
-                for (var i = 1; i <= $scope.numOfBulbs; ++i) {
+                for (var i = 1; i <= $scope.numOfPeople; ++i) {
+
                     for (var j = 0; j < $scope.numOfBulbs; ++j) {
 
                         if (((j + 1) % i) == 0) {
@@ -28,19 +33,15 @@
                     } // for (var j...)
                 } // for(var i...)
 
-                var bulbState = '';
                 var on_count = 0, off_count = 0;
 
                 for (var x = 0; x < $scope.numOfBulbs; ++x) {
                     if ($scope.bulbs[x]) {
-                        bulbState = "O";
                         ++on_count;
                     } else {
-                        bulbState = "X";
                         ++off_count;
                     }
 
-                    console.log("bulb " + (x + 1) + ": " + bulbState);
                 } // for (var x...)
                 console.log("on : " + on_count + "   off : " + off_count);
 
