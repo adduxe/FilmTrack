4223ac29a6d4e90672af8403846435727ce59538
Sonny <adduxe@hotmail.com>
2019-06-28 13:11:26 -0700

    Updated RouteConfig.cs
    To default the app showing the FilmTrack home page.

--- FilmTrackTest/App_Start/RouteConfig.cs
+++ FilmTrackTest/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace FilmTrackTest
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "FilmTrack", id = UrlParameter.Optional }
             );
         }
     }
